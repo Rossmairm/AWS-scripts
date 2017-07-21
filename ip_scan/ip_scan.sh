0cf648784adaeb0b38f32194cc558a106879f2b9
Mark Rossmair <tuc72616@temple.edu>
2017-07-21 15:20:51 -0400

    removed region as it wasn't necessary

--- ip_scan/ip_scan.sh
+++ ip_scan/ip_scan.sh
@@ -7,8 +7,8 @@ for i in $(cat envs); do
 done
 
 for j in $(cat envs);do
-    aws ec2 describe-network-interfaces --region us-east-1 --profile $j | jq '.NetworkInterfaces | .[] | .PrivateIpAddresses | .[] | .PrivateIpAddress' | cut -d'"' -f2 >> $j.txt
-    aws ec2 describe-network-interfaces --region us-east-1 --profile $j | jq '.NetworkInterfaces | .[] | .PrivateIpAddresses | .[] | .Association.PublicIp'| cut -d'"' -f2 | grep -v null >> public.txt
+    aws ec2 describe-network-interfaces --profile $j | jq '.NetworkInterfaces | .[] | .PrivateIpAddresses | .[] | .PrivateIpAddress' | cut -d'"' -f2 >> $j.txt
+    aws ec2 describe-network-interfaces --profile $j | jq '.NetworkInterfaces | .[] | .PrivateIpAddresses | .[] | .Association.PublicIp'| cut -d'"' -f2 | grep -v null >> public.txt
     python ~/Dev/Nessus-Scripts/asset_upload.py $j
 done
 
