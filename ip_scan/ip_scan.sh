90fa5f46370eba89139f745ed441f5db635f920a
Mark Rossmair <tuc72616@temple.edu>
2017-07-21 16:34:22 -0400

    Added support for regions and defining asset_upload path

--- ip_scan/ip_scan.sh
+++ ip_scan/ip_scan.sh
@@ -1,15 +1,39 @@
 #!/bin/bash
 
 echo "" > public.txt
+if [ "$#" -gt  2 ];then
+    echo ""
+    echo "Usage: ./ip_scan.sh {path-to-region-list} {OPTIONAL Path-to-asset_upload.py}"
+    echo ""
+    echo "Region file path can be replaced with the word 'default' to use AWS conf defaults"
+    echo ""
+    exit
+fi
+
+
+
+if [ -z $2 ];then
+    path=~/Dev/Nessus-Scripts/asset_upload.py
+    echo "Path set to " $path
+else
+    path=$2
+fi
+
 
 for i in $(cat envs); do
     echo "" > $i.txt
 done
 
 for j in $(cat envs);do
-    aws ec2 describe-network-interfaces --profile $j | jq '.NetworkInterfaces | .[] | .PrivateIpAddresses | .[] | .PrivateIpAddress' | cut -d'"' -f2 >> $j.txt
-    aws ec2 describe-network-interfaces --profile $j | jq '.NetworkInterfaces | .[] | .PrivateIpAddresses | .[] | .Association.PublicIp'| cut -d'"' -f2 | grep -v null >> public.txt
-    python ~/Dev/Nessus-Scripts/asset_upload.py $j
+    if [ -z $1 ] || [ "$1" == "default"  ]; then
+        aws ec2 describe-network-interfaces --profile $j | jq '.NetworkInterfaces | .[] | .PrivateIpAddresses | .[] | .PrivateIpAddress' | cut -d'"' -f2 >> $j.txt
+        aws ec2 describe-network-interfaces --profile $j | jq '.NetworkInterfaces | .[] | .PrivateIpAddresses | .[] | .Association.PublicIp'| cut -d'"' -f2 | grep -v null >> public.txt
+    else
+        for r in $(cat $1);do
+            aws ec2 describe-network-interfaces --region $r --profile $j | jq '.NetworkInterfaces | .[] | .PrivateIpAddresses | .[] | .PrivateIpAddress' | cut -d'"' -f2 >> $j.txt
+            aws ec2 describe-network-interfaces --region $r --profile $j | jq '.NetworkInterfaces | .[] | .PrivateIpAddresses | .[] | .Association.PublicIp'| cut -d'"' -f2 | grep -v null >> public.txt
+        done
+    fi
+python $path $j
 done
-
-python ~/Dev/Nessus-Scripts/asset_upload.py public
+python $path public
