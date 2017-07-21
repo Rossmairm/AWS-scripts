7d3a30c230b5e6bb588e63291bfb369a09abd0ad
Mark Rossmair <mark@Marks-MacBook-Pro.local>
2017-07-21 11:50:53 -0400

    IP scan complete

---
+++ ip_scan/ip_scan.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "" > public.txt
+
+for i in $(cat envs); do
+    echo "" > $i.txt
+done
+
+for j in $(cat envs);do
+    aws ec2 describe-network-interfaces --region us-east-1 --profile $j | jq '.NetworkInterfaces | .[] | .PrivateIpAddresses | .[] | .PrivateIpAddress' | cut -d'"' -f2 >> $j.txt
+    aws ec2 describe-network-interfaces --region us-east-1 --profile $j | jq '.NetworkInterfaces | .[] | .PrivateIpAddresses | .[] | .Association.PublicIp'| cut -d'"' -f2 | grep -v null >> public.txt
+    python ~/Dev/Nessus-Scripts/SC5APIDemo.py $j
+done
+
+python ~/Dev/Nessus-Scripts/SC5APIDemo.py public
